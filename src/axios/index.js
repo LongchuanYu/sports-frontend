e2cf4d808e6488494de0c35da50c5349a3dd769a
ubuntu <ubuntu@localhost.localdomain>
2021-01-10 23:12:13 +0800

    revise axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import Alerts from '@/components/Alerts'
 import {getDvaApp} from 'umi'
-axios.defaults.baseURL = 'http://192.168.1.143:5000/'
+//axios.defaults.baseURL = 'http://192.168.1.143:5000/'
 // axios.defaults.baseURL = 'http://localhost:5000/'
+axios.defaults.baseURL = 'http://49.235.109.170:5000/'
 
 axios.defaults.timeout = 5000
 
